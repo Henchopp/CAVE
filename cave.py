fd9c5a2e7a8069641d3402c9c1951e0ea825bdfc
prinse1545 <philipp.msrivastava@gmail.com>
2022-03-07 11:25:50 -0600

    more debugging

--- cave.py
+++ cave.py
@@ -211,7 +211,7 @@ class CAVE(torch.nn.Module):
 					return -1.0 * (self.func.fx(a * x + b) - self.func.high) + low
 				elif high != None:
 					return self.func.fx(a * x + b) - self.func.high + high
-		print("FINAL", a, b)
+		print("FINAL", a * x, b)
 		return self.func.fx(a * x + b)
 
 
