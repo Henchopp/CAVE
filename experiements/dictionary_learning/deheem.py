e6604e241115995ba804b339e5315d270f108c32
prinse1545 <philipp.msrivastava@gmail.com>
2022-04-05 10:38:37 -0500

    started dictionary learning

---
+++ experiements/dictionary_learning/deheem.py
@@ -0,0 +1,21 @@
+import torch
+import h5py
+import os
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def train(xrf_path, thresh):
+	print('Reading data...')
+​
+	with h5py.File(xrf_path, "r") as hf:
+		xrf = torch.from_numpy(hf["data"][:]) # reaing
+		xrf = xrf.permute(0,2,1).unsqueeze(1).float()
+		xrf[xrf < 0.0] = 0.0
+		xrf = xrf.round()
+​
+	inds = xrf.sum(dim = [1,2,3]) > thresh # getting indices from sum dist
+    xrf = xrf[inds,:,:,:]
+    print(xrf.shape)
+
+if(__name__ == "__main__"):
+    train("/cave/deheem_orig.h5", 10)
