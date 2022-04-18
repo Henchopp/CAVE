cded64f8b1be21aafaf26d46d381f2a5dcdc1e1b
prinse1545 <philipp.msrivastava@gmail.com>
2022-04-18 13:11:28 -0500

    parameters set

--- experiements/dictionary_learning/deheem.py
+++ experiements/dictionary_learning/deheem.py
@@ -29,15 +29,15 @@ def train(xrf_path, thresh, M, epochs = 100):
 
     global_min = F.poisson_nll_loss(xrf, xrf, log_input = False).item() # getting global min to make minimum loss 0 later
 
-    D_test = torch.from_numpy(np.array(pd.read_csv("/home/prs5019/cave/cave_data/D_raster0,05_all.csv", sep = " ").values, dtype = np.float64))
+    D_data = torch.from_numpy(np.array(pd.read_csv("/home/prs5019/cave/cave_data/D_raster0,05_all.csv", sep = " ").values, dtype = np.float64)).to)device
 
     with h5py.File("/home/prs5019/cave/cave_data/A_raster0,05_all.h5") as hf:
 
-        A_test = torch.from_numpy(np.array(hf["data"][:], dtype = np.float64))
-    print(D_test.shape, A_test.shape)
-    D = torch.nn.Parameter(data = torch.rand(xrf.shape[0], M, device = device), requires_grad = True)
-    A = torch.nn.Parameter(data = torch.rand(M, xrf.shape[1], device = device), requires_grad = True)
-    print(D.shape, A.shape)
+        A_data = torch.from_numpy(np.array(hf["data"][:], dtype = np.float64)).to(device)
+
+    D = torch.nn.Parameter(data = D_data, requires_grad = True)
+    A = torch.nn.Parameter(data = A_data, device = device), requires_grad = True)
+
     A_v = A.view(A.shape[0], 578, 673)
 
     optimizer = torch.optim.Adam([D, A], lr = 1.0, betas = (0.9, 0.999))
@@ -74,7 +74,7 @@ def train(xrf_path, thresh, M, epochs = 100):
     for e in range(epochs):
 
         optimizer.zero_grad() # zeroing gradients
-        output = torch.matmul(F.softplus(D), F.relu(A) * cave(A, low = 0, high = 1, mean = 0.1, var = 0.1, sparse = True))
+        output = torch.matmul(F.softplus(D), F.relu(A) * cave(A, low = 0, high = 1, mean = 5 / 37, var = 0.12, sparse = True))
 
         # ============ smoothing loss ==============
         l_tv = 0.1
@@ -121,4 +121,4 @@ def train(xrf_path, thresh, M, epochs = 100):
 
 
 if(__name__ == "__main__"):
-    train("/home/prs5019/cave/cave_data/deheem_raster0,05.h5", 10, 100, 50_000)
+    train("/home/prs5019/cave/cave_data/deheem_raster0,05.h5", 10, 37, 50_000)
