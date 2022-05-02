26ee95cb593019052bee294df2e307187a89dc83
prinse1545 <philipp.msrivastava@gmail.com>
2022-05-02 12:50:07 -0500

    fixed

--- experiments/inpainting/preprocess.py
+++ experiments/inpainting/preprocess.py
@@ -16,7 +16,7 @@ if(__name__ == "__main__"):
 
     def clean_dir(dir_name):
         for img in os.listdir(os.path.join(args.directory, dir_name)):
-            print(os.path.join(args.directory, dir_name, img))
+
             width, height = Image.open(os.path.join(args.directory, dir_name, img)).size
 
             if(width < 64 or height < 64):
