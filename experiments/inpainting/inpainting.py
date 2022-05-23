e1549ea50d1c1bcddc5bd37ed44d598418fbedeb
prinse1545 <philipp.msrivastava@gmail.com>
2022-05-23 16:24:26 -0500

    moving to gpu

--- experiments/inpainting/inpainting.py
+++ experiments/inpainting/inpainting.py
@@ -56,7 +56,7 @@ def train(epochs = 1000, cave = False):
     optimizer = torch.optim.Adam(model.parameters(), lr = 1.0e-4, betas = (0.9, 0.999))
 
     # =================== perceptual loss =================
-    vgg = vgg16(pretrained = True).features[: 22]
+    vgg = vgg16(pretrained = True).features[: 22].to(device)
 
     def perceptual_loss(y_, y):
         return F.mse_loss(vgg(y_), vgg(y))
